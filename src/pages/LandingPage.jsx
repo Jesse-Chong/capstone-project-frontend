759bb340f7e132ff6ee5b890a2d6855519d91652
jameel914 <jameelibrahim107@gmail.com>
2024-03-05 10:29:29 -0500

    Added footer in LandingPage

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -130,6 +130,37 @@ function LandingPage() {
           <div class="col">Column</div> */}
         </div>
       </div>
+      <footer
+        className="text-center text-white"
+        style={{ backgroundColor: "#45637d" }}
+      >
+        <div className="container p-4">
+          <section className="">
+            <div className="row d-flex justify-content-center">
+              <div className="col-lg-6">
+                <div className="ratio ratio-16x9">
+                  <iframe
+                    className="shadow-1-strong rounded"
+                    src="https://www.youtube.com/embed/vlDzYIIOYmM"
+                    title="YouTube video"
+                    allowfullscreen
+                  ></iframe>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+
+        <div
+          className="text-center p-3"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+        >
+          © 2024 Copyright:
+          <a className="text-white" href="https://mdbootstrap.com/">
+            FreshStart.com
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
