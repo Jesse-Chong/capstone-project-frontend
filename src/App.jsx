aa441ac6f678aa99eb0e3dca3403bb4011d023a9
Jesse Chong <jessechong@pursuit.org>
2024-03-05 12:48:07 -0500

    Added pages to App.jsx and debugged

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LandingPage from "./pages/LandingPage";
 import ShowDetailsPage from "./pages/ShowDetailsPage";
@@ -7,12 +8,19 @@ import ShowDetailsPage from "./pages/ShowDetailsPage";
 
 function App() {
   return (
-    <>
-      <LandingPage />
-      <HomePage/>
-      <ShowDetailsPage />
-      
-    </>
+<Router>
+        {/* <Navbar /> */}
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/details" element={<ShowDetailsPage />} />
+          {/* <Route path="/foods" element={<IndexPage />} />
+          <Route path="/foods/new" element={<NewPage />} />
+          <Route path="/foods/:index" element={<ShowPage />} />
+          <Route path="/foods/:index/edit" element={<EditPage />} />
+          <Route path="*" element={<PageNotFound />} /> */}
+        </Routes>
+      </Router>
   );
 }
 
